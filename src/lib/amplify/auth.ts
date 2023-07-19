63354faef293fbaada0c204c5d4956425077f49e
TA-iida-d <d-iida@tri-ash.com>
2023-07-19 23:33:57 +0000

    feat: sign up sign out

---
+++ src/lib/amplify/auth.ts
@@ -0,0 +1,34 @@
+import { Auth } from "aws-amplify";
+
+type SignUpParameters = {
+  username: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+};
+
+export async function signUp({
+  username,
+  password,
+  email,
+  phoneNumber,
+}: SignUpParameters) {
+  try {
+    const { user } = await Auth.signUp({
+      username,
+      password,
+      attributes: {
+        email, // optional
+        phoneNumber, // optional - E.164 number convention
+        // other custom attributes
+      },
+      autoSignIn: {
+        // optional - enables auto sign in after user is confirmed
+        enabled: true,
+      },
+    });
+    console.log(user);
+  } catch (error) {
+    console.log("error signing up:", error);
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,62 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+import { Amplify, Auth } from "aws-amplify";
+
+import awsConfig from "../aws-exports";
+import { FormEventHandler, useCallback, useState } from "react";
+
+Amplify.configure(awsConfig);
+
+export default function App() {
+  const [user, setUser] =
+    useState<Awaited<ReturnType<(typeof Auth)["signUp"]>>["user"]>();
+
+  const handleSignOut = useCallback(() => {
+    Auth.signOut({ global: true });
+    setUser(undefined);
+  }, []);
+
+  const handleSignIn = useCallback<FormEventHandler<HTMLFormElement>>(
+    async (e) => {
+      e.preventDefault();
+
+      const form = e.currentTarget;
+      const email = (form.elements.namedItem("email") as HTMLInputElement)
+        .value;
+      const password = (form.elements.namedItem("password") as HTMLInputElement)
+        .value;
+
+      console.log({ email, password });
+
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: { email },
+        autoSignIn: { enabled: true },
+      });
+
+      setUser(user);
+    },
+    []
+  );
+
+  if (user) {
+    return (
+      <button type="button" onClick={handleSignOut}>
+        Sign out
+      </button>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSignIn}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 }
